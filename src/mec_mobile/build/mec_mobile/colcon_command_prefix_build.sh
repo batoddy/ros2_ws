14b8ac1ec79b16279aee7fe8014f30237fa9d66c
batoddy <batuhanodcikin@gmail.com>
2025-08-20 03:19:32 +0300

    rgbd & imu & parkour map - gazebo sim with rviz2

---
+++ src/mec_mobile/build/mec_mobile/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/batoddy/Desktop/ros2_ws/src/mec_mobile/install/mec_mobile_description/share/mec_mobile_description/package.sh"
